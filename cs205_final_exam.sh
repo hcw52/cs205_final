904d7b98d96b3b023f8a4c3c984518edfff3f9fb
Hunter Woodruff <89612520+hcw52@users.noreply.github.com>
2021-11-07 14:11:45 -0700

    Add files via upload
    Modified cs205_final_exam.sh to print total number of pokemon and the mean hp and attack of said pokemon

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,11 +1,28 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
-# ======= SUMMARY OF POKEMON.DAT ======
-#    Total Pokemon: [VALUE]
-#    Avg. HP: [VALUE]
-#    Avg. Attack: [VALUE]
-# ======= END SUMMARY =======
+# 001 
+# Woodruff
+# Hunter
+# hcw52
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#Counts the number of lines in the file subtracts headers for total number of pokemon
+sum+=1
 
-# The "Avg." values should be calculated as mean values for the corresponding columns.
-# The spacing and header formatting should match the above formatting description exactly.
-# There should be a comment explaining the purpose of each line in your shell script. 
-# The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#Fines Mean hp of pokemon
+hp+=($6)
+hpMean = hp/(sum-1)
+
+#Fines mean of attack values
+attack+=($7)
+attackMean = attack/(sum-1)
+}
+}
+END{
+# Prints the number of lines in the file
+print "======= SUMMARY OF POKEMON.DAT ======"
+print "Total Pokemon: " sum
+print "Avg. HP: " hpMean
+print "Avg. Attack: " attackMean
+print "======= END SUMMARY ======="}
+
